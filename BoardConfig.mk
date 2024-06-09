78f69c292a803f7e4b5292624588e651fd11c913
q67aaa <q67aaa@gmail.com>
2024-06-09 05:55:26 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-LE7
+DEVICE_PATH := device/tecno/LE7
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
