73186b15b6f5855e81a9868f47ee413873a2e804
q67aaa <q67aaa@gmail.com>
2024-06-09 05:55:53 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/TECNO-LE7
+LOCAL_PATH := device/tecno/LE7
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
